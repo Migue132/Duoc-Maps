6365b62bd7151b84c0b6f147004eee782184ac54
PeLuFe <111013371+PeLuFe@users.noreply.github.com>
2024-11-04 20:18:22 -0300

    Delete Proyecto/Web/src/pages/PuntoEstudiantil directory

--- Proyecto/Web/src/pages/PuntoEstudiantil/PuntoEstudiantil.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-    selector: 'PuntoEstudiantil',
-    templateUrl: './PuntoEstudiantil.component.html',
-    styleUrls: ['./PuntoEstudiantil.component.css']
-})
-export class PuntoEstudiantilComponent{
-
-}
