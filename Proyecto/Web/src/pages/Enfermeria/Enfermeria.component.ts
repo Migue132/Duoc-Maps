f826d3384de3c50ce918c7ded3ca40235d78cfff
PeLuFe <111013371+PeLuFe@users.noreply.github.com>
2024-11-04 20:18:01 -0300

    Delete Proyecto/Web/src/pages/Enfermeria directory

--- Proyecto/Web/src/pages/Enfermeria/Enfermeria.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-    selector: 'Enfermeria',
-    templateUrl: './Enfermeria.component.html',
-    styleUrls: ['./Enfermeria.component.css']
-})
-export class EnfermeriaComponent{
-    
-}
