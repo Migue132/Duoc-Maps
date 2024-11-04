aa9f69c10456efcb98767da79217df129116a3fd
PeLuFe <111013371+PeLuFe@users.noreply.github.com>
2024-11-04 20:18:13 -0300

    Delete Proyecto/Web/src/pages/Finanzas directory

--- Proyecto/Web/src/pages/Finanzas/Finanzas.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-   selector: 'Finanzas',
-   templateUrl: './Finanzas.component.html',
-   styleUrls: ['./Finanzas.component.css']  
-})
-export class FinanzasComponent{
-    
-}
