02507b3292c174ba2cf9ed1b04ac6c32b4315ebf
PeLuFe <111013371+PeLuFe@users.noreply.github.com>
2024-11-04 20:17:48 -0300

    Delete Proyecto/Web/src/pages/Biblioteca directory

--- Proyecto/Web/src/pages/Biblioteca/Biblioteca.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-    selector: 'Biblioteca',
-    templateUrl: './Biblioteca.component.html',
-    styleUrls: ['./Biblioteca.component.css']
-})
-export class BibliotecaComponent{
-    
-}
