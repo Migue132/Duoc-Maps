debebae82a826056a5665a205cfc047ec56b4f2b
PeLuFe <111013371+PeLuFe@users.noreply.github.com>
2024-11-04 20:18:33 -0300

    Delete Proyecto/Web/src/pages/food Place directory

--- Proyecto/Web/src/pages/food Place/foodPlace.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-    selector: 'food-place',
-    templateUrl: './foodPlace.component.html',
-    styleUrls: ['./foodPlace.component.css']
-})
-export class FoodPlaceComponent{
-
-}
